a225fe86cb81191726a826f8b8139948ca6e976e
Andrew <pisankin96@mail.ru>
2020-05-03 15:51:04 +0700

    Configured built-in authorization system

--- PcComponentsStore/PcComponentsStore/settings.py
+++ PcComponentsStore/PcComponentsStore/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +53,7 @@ ROOT_URLCONF = 'PcComponentsStore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,17 +68,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'PcComponentsStore.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'PcComponentsDB',
+        'USER': 'postgres',
+        'PASSWORD': '12312378',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
+AUTH_USER_MODEL = 'users.CustomUser'
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -99,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -112,7 +118,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
--- PcComponentsStore/PcComponentsStore/urls.py
+++ PcComponentsStore/PcComponentsStore/urls.py
@@ -14,8 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from basic import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.homeView),
+    path('users/', include('users.urls')),
+    path('users/', include('django.contrib.auth.urls')),
+
+    # path('', HomeView.as_view(), name='home'),
+    # path('about/', AboutView.as_view(), name='about'),
+    # path('catalog/', CatalogView.as_view(), name='catalog'),
+    # path('checkout/', CheckoutView.as_view(), name='checkout'),
+    # path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
+    # path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    # path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    # path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    # path('remove-item-from-cart/<slug>/', remove_single_item_from_cart,
+    #      name='remove-single-item-from-cart'),
 ]
